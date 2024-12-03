f22afeeaddc71ddc5007866f3e08dd1d0013621e
shreyashchandra <shreyash.chandra123@gmail.com>
2024-12-03 23:18:24 +0530

    Update node-fetch version and add nodemon; modify cron job logging

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mainRoute = require("./routes/index");
 const { dbConnect } = require("./db");
 var cors = require("cors");
 require("dotenv").config();
-var cron = require("node-cron");
+const cron = require("node-cron");
 const fetch = require("node-fetch");
 
 const app = express();
@@ -18,9 +18,7 @@ dbConnect();
 cron.schedule("*/20 * * * * *", async () => {
   try {
     const res = await fetch(`https://paynow-backend.onrender.com/api/v1/user/`);
-    console.log(
-      `Ping successful: ${res.status} - ${new Date().toLocaleTimeString()}`
-    );
+    console.log("hello from cron");
   } catch (error) {
     console.error("Ping failed:", error.message);
   }
